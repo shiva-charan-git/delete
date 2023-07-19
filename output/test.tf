1aecdc4e34334bd657a92191a6a5e4c2de6e4fed
shiva-charan-git <shivacharan021@gmail.com>
2023-07-19 09:57:32 +0530

    M

--- output/test.tf
+++ output/test.tf
@@ -1,5 +1,15 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-06ca3ca175f37dd66"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0827c01ee4ba4d8aa"]
+  tags = {
+    name = "test"
+  }
+}
+
+
 output "test" {
-    
-    value = "Terraform"
+
+    value = aws_instance.ec2.public_ips
   
 }
